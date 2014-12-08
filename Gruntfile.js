6aad849a5b53795dc3dbbf576d1dc302c6a42242
yasser <ytoruno@gmail.com>
2014-12-08 16:22:52 -0500

    grunt: uglify compiled browserify file as part of default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function(grunt) {
       },
       my_target: {
         files: {
-          'js/build/tetronimo.js': ['js/*.js'],
+          'js/build/tetris.min.js': ['js/build/tetris.js'],
         }
       }
     },
@@ -59,5 +59,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-browserify');
 
-  grunt.registerTask('default', ['jshint', 'browserify']);
+  grunt.registerTask('default', ['jshint', 'browserify', 'uglify']);
+  grunt.registerTask('compile', ['default']);
 };
